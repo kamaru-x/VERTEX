4a67911cf56223c162fd9b8b9fe2a1ca51ac73ec
KAMARU <xin.kamaru@gmail.com>
2023-01-26 10:31:30 +0530

    /// admin login completed ///

--- u_auth/urls.py
+++ u_auth/urls.py
@@ -3,4 +3,6 @@ from u_auth import views
 
 urlpatterns = [
     path('',views.signin,name='sign-in'),
+    path('dashboard/',views.dashboard,name='dashboard'),
+    path('sign-out/',views.signout,name='sign-out')
 ]
--- u_auth/views.py
+++ u_auth/views.py
@@ -1,6 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,logout,login
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required,user_passes_test
 
 # Create your views here.
 
+#################################################################################
+
 def signin(request):
+    if request.method == 'POST' :
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        user = authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            return redirect('dashboard')
+        else:
+            messages.error(request,'incorrect username or password')
+            return redirect('.')
     return render(request,'login.html')
+
+#################################################################################
+
+@user_passes_test(lambda u: u.is_superuser)
+def dashboard(request):
+    return render(request,'index.html')
+
+#################################################################################
+
+@user_passes_test(lambda u: u.is_superuser)
+def signout(request):
+    logout(request)
+    return redirect('sign-in')
